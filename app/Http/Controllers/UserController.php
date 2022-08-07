aae7e7a9d31465fddd6ec994a58d3fad8807de99
Omar Akram <76477597+3Akram2@users.noreply.github.com>
2022-08-07 16:44:51 +0200

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Users = json_decode(\Illuminate\Support\Facades\File::get(storage_path('users.json')),true);
+        return (view('users.index', ['Users'=>$Users]));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return (view('users.create'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return (view('users.store',['req'=>$request]));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $users = json_decode(\Illuminate\Support\Facades\File::get(storage_path('users.json')),
+        true);
+            $user=$users[$id-1];
+            return view('users.edit')->with( [ "user" => $user  ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        return dd($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $users = json_decode(\Illuminate\Support\Facades\File::get(storage_path('users.json')),
+        true);
+        $user=$users[$id-1];
+
+            return 'You removed user with id '.$user['id'].'
+            from storage.
+            ';
+
+
+    }
+}
